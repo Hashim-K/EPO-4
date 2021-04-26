59a05b5811e91a1f384ab216ad28cb3c9a09be1c
Hashim Karim <hashimkarim168@gmail.com>
2021-04-26 16:43:36 +0200

    made calcA an external function

---
+++ GUI and control/calcA.m
@@ -0,0 +1,6 @@
+  function a = calcA(f, alpha)
+        Fr=150;
+        m=13;
+        a(1)= (f-Fr)*cos(alpha)/m;
+        a(2)= (f-Fr)*sin(alpha)/m;
+        ends
--- GUI and control/car.m
+++ GUI and control/car.m
@@ -146,13 +146,5 @@ classdef car
                 stat = obj.status;
     end
     
-    %physics modelling
-    function a = calcA(f, alpha)
-        Fr=150;
-        m=13;
-        a(1)= (f-Fr)*cos(alpha)/m;
-        a(2)= (f-Fr)*sin(alpha)/m;
-    end
-    
 end
 end
