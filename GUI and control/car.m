1f2bd62ee1efd23b99d134cb1c21fc78c75aff56
Hashim Karim <hashimkarim168@gmail.com>
2021-04-26 17:16:08 +0200

    Fixed display method

--- GUI and control/car.m
+++ GUI and control/car.m
@@ -9,7 +9,6 @@ classdef car
     angle %steering angle
 	time
     status; % -1 = crash, 0 = off, 1 = on, 2=success
-    statustext=["crash", "off", "on", "success"]
     end
 
     methods
@@ -36,6 +35,7 @@ classdef car
 	    % disp prints the object's values
 	    % first update position based on current time and velocity
         global x
+        statustext=["crash", "off", "on", "success"]
 	    x = obj.position;
 	    v = obj.velocity;
         a = obj.acceleration;
