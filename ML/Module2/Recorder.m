a59869da4ea987a8e35571f697561def24221fb5
Hashim Karim <hashimkarim168@gmail.com>
2021-04-26 17:39:13 +0200

    Fixed recorder

--- ML/Module2/Recorder.m
+++ ML/Module2/Recorder.m
@@ -4,4 +4,12 @@ recObj = audiorecorder(Fs,16,1); % create audio object, 16 bits resolution
 disp('Start speaking...')
 recordblocking(recObj, 2); % do a 2 second recording (blocking)
 disp('End of Recording.');
-Right(:,10) = getaudiodata(recObj);
+dir=lower(input("What direction?: [Left/Right/Up/Down]", 's'))
+if dir=="left"
+    Left(:,10) = getaudiodata(recObj);
+if dir=="right"
+    Right(:,10) = getaudiodata(recObj);
+if dir=="up"
+    Up(:,10) = getaudiodata(recObj);
+if dir=="down"
+    Down(:,10) = getaudiodata(recObj);
