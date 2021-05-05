afbd3caee83fb8b70281af09deda80b709f38c91
Floris van Mourik <florisvanmourik@outlook.com>
2021-05-05 23:44:34 +0200

    Update controller
    improved algorithm and added margins

--- module 3/controller.m
+++ module 3/controller.m
@@ -3,31 +3,35 @@ c = 0.1;
 m = 5.6;
 Fai = 10;
 Fbi = 10;
-distance = 6;
+distance = 4.96;
+margin = 0.02;
+
+
+%start_pos = 1;
+%stop_pos = 2.28;
 
 
 Fa = Fai;
 Fb = 0;
 v0 = 0;
 s0 = 0;
-output = sim('KITTDynamicsFinal','StartTime','0','StopTime','6','FixedStep','0.01');
+output = sim('KITTDynamicsFinal','StartTime','0','StopTime','8','FixedStep','0.01');
 
 
 Fa = 0;
 Fb = Fbi;
 
 s = 0;
-v0 = 1;
-while(s < distance - 0.01)%find fit to position
+v0 = 0.1;
+while(s < distance - 0.004)%find fit to position
     i = 1;
     output1 = sim('KITTDynamicsFinal','StartTime','0','StopTime','8','FixedStep','0.01');
     while(output1.v.signals.values(i,1) > 0.01) %searching untill velocity is more then v
         i = i+1;
     end
-    disp(i);
     
-    s = output1.s.signals.values(i-1,1);
-    v0 = v0 + (distance - s)*0.7;
+    s = output1.s.signals.values(i,1);
+    v0 = v0 + (distance - s)*0.6;
 
 plot(output1.s.signals.values, output1.v.signals.values);
 hold on;
@@ -45,10 +49,12 @@ hold off;
 i = 1;
 
 
-while(output.v.signals.values(i,1) < vref) %searching untill velocity is more then v
+while(output.v.signals.values(i,1) < v_max) %searching untill velocity is more then v
     i = i+1;
 end
 
-stop_t = output.v.time(i-1,1)
+stop_t = output.v.time(i,1)
+
+s = x_stop - v_max*0.03 - margin %correct for maximum sensor delay (30 ms * max velocity) and adda margin
 
 
