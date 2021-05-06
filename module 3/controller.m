1c1ccb6162c7476e37fa0e6fb38593f8ebc4de2d
Floris van Mourik <florisvanmourik@outlook.com>
2021-05-06 10:35:27 +0200

    updated controleer and gui
    -

--- module 3/controller.m
+++ module 3/controller.m
@@ -1,23 +1,22 @@
-%function distance_to_wall = controller(Fai, Fbi, distance, timer_period, margin)
+function [distance_to_wall, stop_t, v_max] = controller(Fai, Fbi, distance, timer_period, margin)
+
+
 
 b = 5;
 c = 0.1;
 m = 5.6;
-Fai = 10;
-Fbi = 10;
-
-distance = 5;
-margin = 0.02;
-timer_period = 0.1;
-
-
-
+% Fai = 10;
+% Fbi = 10;
+% 
+% distance = 5;
+% margin = 0.02;
+% timer_period = 0.1;
 
 Fa = Fai;
 Fb = 0;
 v0 = 0;
 s0 = 0;
-output = sim('KITTDynamicsFinal','StartTime','0','StopTime','8','FixedStep','0.01');
+output = sim('KITTDynamicsFinal', 'StartTime','0','StopTime','8','FixedStep','0.01', 'SrcWorkspace','current');
 
 
 Fa = 0;
@@ -27,7 +26,7 @@ s = 0;
 v0 = 0.1;
 while(s < distance - 0.004)%find fit to position
     i = 1;
-    output1 = sim('KITTDynamicsFinal','StartTime','0','StopTime','8','FixedStep','0.01');
+    output1 = sim('KITTDynamicsFinal', 'StartTime','0','StopTime','8','FixedStep','0.01', 'SrcWorkspace','current');
     while(output1.v.signals.values(i,1) > 0.01) %searching untill velocity is more then v
         i = i+1;
     end
@@ -47,7 +46,7 @@ title('Velocity of the car');
 xlabel('Distance [m]') 
 ylabel('velocity [m/s]') 
 hold off;
-[x_stop,v_max] = polyxpoly(output.s.signals.values, output.v.signals.values,output1.s.signals.values, output1.v.signals.values)
+[x_stop,v_max] = polyxpoly(output.s.signals.values, output.v.signals.values,output1.s.signals.values, output1.v.signals.values);
 i = 1;
 
 
@@ -55,9 +54,9 @@ while(output.v.signals.values(i,1) < v_max) %searching untill velocity is more t
     i = i+1;
 end
 
-stop_t = output.v.time(i,1)
+stop_t = output.v.time(i,1);
 
-s = x_stop - v_max*0.03 - timer_period*v_max - margin %correct for maximum sensor delay (30 ms * max velocity) and adda margin
+s = x_stop - v_max*0.03 - timer_period*v_max - margin; %correct for maximum sensor delay (30 ms * max velocity) and adda margin
 
-distance_to_wall = 6 - s %this is the correct coordinate system
-%end
+distance_to_wall = 6 - s; %this is the correct coordinate system
+end
---
+++ module 3/test.m
@@ -0,0 +1,3 @@
+clc;
+clear all;
+[x,t,v] = controller(10, 10, 5, 0.1, 0.02)
