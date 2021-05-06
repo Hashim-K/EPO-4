43e0d79bbbee67c74ba0867a75251b9c5b35e996
Hashim Karim <hashimkarim168@gmail.com>
2021-05-06 12:04:07 +0200

    Update to Soji Car Model

--- remove_folder_later/car_model.m
+++ remove_folder_later/car_model.m
@@ -5,19 +5,35 @@ classdef car_model  < handle
         b = 5; %Nm/s
         c = 0.1; %Nm/s^2
         local_time;
+        start_time 
         y = [];
         t = [];
-        tspan = [0 20];
+        tspan = [0 100];
         ode_length =0;
-    end 
+
+    end   
     methods
-        
-        function obj =init(obj )
+        function disp(obj) 
+            x = obj.get_pos(); 
+            velocity = obj.get_velocity();
+            total_time = obj.get_total_time(); 
+            update_time = obj.get_update_time();
+            
+            disp("Position = " + x);
+            disp("Velocity = " + velocity); 
+            disp("Total time = " + total_time); 
+            disp("Update time = " + update_time); 
+
+        end 
+        function obj =init(obj,x_pos)
             obj.local_time = get_time();
-            y0 = [0 0];
+            obj.start_time = get_time(); 
+            y0 = [x_pos 0];         % intial conditions 
             [obj.t,obj.y] = ode45(@(t,y) motion_ode(t,y,obj.Fa), obj.tspan, y0);
             obj.ode_length= length(obj.t);
+            plot(obj.t,obj.y);
         end
+        
         function update_ode(obj) 
             x0 = obj.get_pos;
             x1 = obj.get_velocity; 
@@ -28,18 +44,29 @@ classdef car_model  < handle
                               %absolute time is not relevant
             [obj.t,obj.y] = ode45(@(t,y) motion_ode(t,y,obj.Fa), obj.tspan, y0);
             obj.ode_length= length(obj.t);
-           % plot(obj.t,obj.y)
+           plot(obj.t,obj.y);
         end 
+        
         function x = get_pos(obj) 
-            dt = get_time()-obj.local_time
-            index = round(obj.ode_length/20 * dt)
-            x = obj.y(index,2); 
+            dt = get_time()-obj.local_time;
+            [c index] = min(abs(obj.t-dt));
+            x = obj.y(index,1); 
         end 
+        
         function v = get_velocity(obj) 
             dt = get_time()-obj.local_time
-            index = round(length(obj.t)/20 * dt);
-            v = obj.y(index,1); 
+            [c index] = min(abs(obj.t-dt));
+            v = obj.y(index,2); 
         end
+        
+        function t = get_total_time(obj)
+            t = get_time() - obj.start_time;
+        end 
+        
+        function t = get_update_time(obj)
+            t = get_time() - obj.local_time;
+        end 
+            
         function set_force(obj,force)
             %Only when the force changes do we need to recompute the ode;
             if(force ~= obj.Fa) 
