1ee8b2b54c66409f8abcc3806a432c224ea8fc18
Calvin W <c.r.wijnveen@student.tudelft.nl>
2021-05-21 10:22:50 +0200

    with micdata you can create realistic mic recordings

--- EPO_Part2/Module4/NewFiles/main.m
+++ EPO_Part2/Module4/NewFiles/main.m
@@ -1,14 +1,17 @@
 clear all;
-p = [0 0 468 468 ; 0 470 470 0; 0 0 0 0];
+p = [0 0 468 468 234; 0 470 470 0 0; 0 0 0 0 0];
 % p = [0 600; 0 0; 0 0];
-carpos = [400; 255];
-Fs = 40000;
-b = binit(Fs, -1, -1, -1, "");
-load('measure100_400.mat');
-% [refSig, y] = micdata(b, p, carpos);
-refSig = y(:,5);
-y(:,5) = [];
-% h = decon(refSig, y, 0); %mode 0 = freq domain, mode 1 = match filter
-%[h, beta_hat] = rir_generator_x(343, Fs, (p./100)', (carpos./100)', [6 6 6], 0.5); %perfect impulse generator
+carpos = [246; 5];
+b = binit(-1, -1, -1, -1, "");
+y = micdata(b, p, carpos);
+refSig = micdata(b, p, [1; 1]);
+refSig(:,2:width(p)) = [];
+h = decon(refSig, y, 0); %mode 0 = freq domain, mode 1 = match filter
+% [h, beta_hat] = rir_generator_x(343, Fs, (p./100)', (carpos./100)', [6 6 6], 0.5); %perfect impulse generator
 %if using the above line, carpos must be 3 dimensional.
 % [location, r] = TDOA(h, p, Fs);
+
+for i=1:width(p)
+    subplot(width(p),1,i)
+    plot(h(:,i))
+end
--- EPO_Part2/Module4/TDOA.m
+++ EPO_Part2/Module4/TDOA.m
@@ -19,19 +19,21 @@
 %                 pkloc(1)+searRange,j)) > tolerance*max(abs(h(pkloc(1)...
 %                 -searRange:pkloc(1)+searRange,j))));
 %             end
-%             r(i,j) = 100*(pkloc(1)-pkloc2(1))*(c/Fs);%Conversion from m to 
+%             r(i,j) = 100*(pkloc(1)-pkloc2(1))*(c/Fs);%Conversion from m 
 %             %to cm takes place
 %         end
 %     end
 %     location = localize(r, p);
 % end
-%-------------------------------------%
+
+%-------------------------------------------------------------------------%
+
 %This second version looks for all values greater than the tolerance * max 
 %value and sets any value that isn't greater than the tolerance * the max 
 %value to zero then the peaks are found and the first one is taken.
 function [location, r] = TDOA(h, p, Fs)
     c = 343;
-    tolerance = 0.5;
+    tolerance = 0.1;
     maxDist = sqrt(2)*6;
     searRange = ceil(maxDist*Fs/c);
     N = width(h);
@@ -48,13 +50,13 @@ function [location, r] = TDOA(h, p, Fs)
                 [~, pks2] = findpeaks(abs(compVec2));
             else
                 pkloc2 = abs(h(pks1(1)-searRange:...
-                pks1(1)+searRange,j)) > tolerance*max(abs(h(pkloc(1)...
-                -searRange:pkloc(1)+searRange,j)));
+                pks1(1)+searRange,j)) > tolerance*max(abs(h(pks1(1)...
+                -searRange:pks1(1)+searRange,j)));
                 compVec2 = pkloc2.*h(pks1(1)-searRange:pks1(1)+searRange,j);
                 [~, pks2] = findpeaks(abs(compVec2));
                 pks2 = pks2 + searRange;
             end
-            r(i,j) = 100*(pks1(1)-pks2(1))*(c/Fs);%Conversion from m to 
+            r(i,j) = 100*(pks1(1)-pks2(1))*(c/Fs);%Conversion from m
             %to cm takes place
         end
     end
