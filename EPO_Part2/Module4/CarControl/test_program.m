50e4fcfd4e2b63ad6941e20946747ff83f4f03a2
Floris van Mourik <florisvanmourik@outlook.com>
2021-06-08 14:10:57 +0200

    .

--- EPO_Part2/Module4/CarControl/test_program.m
+++ EPO_Part2/Module4/CarControl/test_program.m
@@ -1,60 +1,50 @@
-kitt = EPOCommunications('open','P'); % create kitt! now in public mo
+radius = 70;
+waypoint_1 = [250,100];
+start = [0,0];
+endpoint = [500,400];
 
 
-waypoints = [0 0; ...
-	100 100; ...
-	200 400; ...
-	600 400];
-refPath = referencePathFrenet(waypoints);
-connector = trajectoryGeneratorFrenet(refPath);
+path = routing(radius, waypoint_1, start, endpoint);
 
-initState = [0 0 0 0 0 0];  % [S ds ddS L dL ddL]
-termState = [1000 0 0 0 0 0]; % [S ds ddS L dL ddL]
-[~,trajGlobal] = connect(connector,initState,termState,5);
-
-% show(refPath);
-% hold on
-% axis equal
-
-% plot(trajGlobal.Trajectory(:,1),trajGlobal.Trajectory(:,2),'b')
-% legend(["Waypoints","Reference Path","Trajectory to 30m"])
-
-x_coor = trajGlobal.Trajectory(:,1);
-y_coor = trajGlobal.Trajectory(:,2);
+x_coor = path(1,:);
+y_coor = path(2,:);
 plot(x_coor,y_coor)
             xlim([0, 600])
             ylim([0, 600])
 hold on;
+plot(waypoint_1(1), waypoint_1(2),'r+');
 
 position = [0,0]
-
-g1 = plot(pathPoints(1), pathPoints(2),'r+');
-g2 = plot(points(1), points(2),'rx');
 
 i = 0;
 k_t = 1;
 k_d = 0.001;
 
-while(i < 10)
+g1 = plot(position(1), position(2),'gx');
+g2 = plot(path(1,1), path(2,1),'gx');
+
+
+while(i < 500)
 i = i + 1
 position = [i,i]
-pathPoints = closestPoint(refPath,position)
+[n, distance_to_path] = closest_point(position, path)
 delete(g1)
 delete(g2)
-g1 = plot(pathPoints(1), pathPoints(2),'r+');
-g2 = plot(position(1), position(2),'rx');
-theta_path = pathPoints(3)/pi*180;
+
+g1 = plot(position(1), position(2),'gx');
+g2 = plot(path(1,n), path(2,n),'gx');
+
+theta_path = path(3,n);
 theta_rover = 45;
 
-distance_to_path = sqrt((pathPoints(1) - position(1))^2+(pathPoints(2) - position(2))^2);
 if(theta_rover^2 < 90^2) %if rover is looking towards right half plane
-    if(position(2) < pathPoints(2)) %then if rover is beneath path steer to left
+    if(position(2) < path(2,n)) %then if rover is beneath path steer to left
         a = 1;
     else
         a = -1;
     end
 else
-    if(position(2) < pathPoints(2))
+    if(position(2) < path(2,n))
         a = -1;
     else
         a = 1;
@@ -68,12 +58,12 @@ if(steering < -45)
     steering = -45;
 end
 steering
-setSteering(steering);
+%setSteering(steering);
 force = 1;
-setMotorSpeed(force);
-pause(0.5)
+%setMotorSpeed(force);
+pause(0.01)
 
 end
-            
+
 
             %plot(app.UIAxes,waypoints(3,1),waypoints(3,2),'r+')
---
+++ GUI and control/raaklijn.m
@@ -0,0 +1,32 @@
+function h = raaklijn(x1,y1,r, Xpoint, Ypoint)
+
+% % % % % PLOT TANGENTS TO A CIRCLE FROM A POINT OUTSIDE THE CIRCLE —
+% % Reference: https://en.wikipedia.org/wiki/Law_of_cosines
+a = linspace(0, 2*pi);                                              % Assign Angle Vector
+                                                             % Circle Radius (‘a’)
+ctr = [x1 y1];                                                    % Circle Centre
+x = ctr(1) + r.*cos(a);                                             % Circle ‘x’ Vector
+y = ctr(2) + r.*sin(a);                                             % Circle ‘y’ Vector
+XY = [Xpoint Ypoint];                                                    % Point Outside Circle From Which Tangents Are Plotted
+c = hypot(XY(1)-ctr(1),XY(2)-ctr(2));
+if c <= r
+    error('\n\tPOINT ON OR WITHIN CIRCLE RADIUS —> NO TANGENTS POSSIBLE!\n\n')
+end
+b = sqrt(c.^2 - r.^2);                                              % See Wikipedia Reference
+alfa = acos((r.^2 - b.^2 - c.^2)./(-2*b*c));                        % See Wikipedia Reference
+beta = acos((b.^2 - r.^2 - c.^2)./(-2*r*c));                        % See Wikipedia Reference
+pt_ctr_angl = atan2(-(XY(2)-ctr(2)),-(XY(1)-ctr(1)));               % Angle From ‘Point’ To Circle Centre
+alfac = [pt_ctr_angl + alfa; pt_ctr_angl - alfa];                   % Angles From ‘Point’ For Tangents
+xtng = XY(1) + [b.*cos(alfac(1)); b.*cos(alfac(2))];                % Tangent Point ‘x’ Coordinates
+ytng = XY(2) + [b.*sin(alfac(1)); b.*sin(alfac(2))];                % Tangent Point ‘y’ Coordinates
+%figure(1)
+%plot(x, y)                                                          % Plot Circle
+%hold on
+%plot(XY(1), XY(2), 'bp')                                            % Plot ‘Point’
+%plot([XY(1) xtng(1)], [XY(2) ytng(1)])                              % Plot Tangents
+%plot([XY(1) xtng(2)], [XY(2) ytng(2)])   %second one always longer imo                           % Plot Tangents
+%hold off
+grid
+axis equal 
+h = [xtng(1), ytng(1); xtng(2), ytng(2)];
+end
